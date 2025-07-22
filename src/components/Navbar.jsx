51ca321b4ecb613b7a1bb6063447ac290bea46f6
murat-yildirim@hotmail.com.tr <murat-yildirim@hotmail.com.tr>
2025-07-22 15:38:30 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,52 +5,63 @@ import { logout } from "./api";
 import { logoutSuccess } from "../redux/redux";
 
 const Navbar = () => {
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const authState = useSelector((store)=> store.auth)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const authState = useSelector((store) => store.auth);
 
   const onLogoutSuccess = async () => {
     try {
-      await logout()
-      dispatch(logoutSuccess())
-      navigate('/')
+      await logout();
+      dispatch(logoutSuccess());
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <div>
       <nav className="navbar bg-primary">
         <div className="container-fluid">
-          <b className="navbar-brand">
-            React App
-          </b>
+          <b className="navbar-brand">React App</b>
           <ul className="navbar-nav">
-          {authState.id === 0 && (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  Sign Up
-                </Link>
-              </li>
-            </>
-          )}
-          {authState.id > 0 && (
-            <>              
-              <li className="nav-item">
-                <span className="nav-link" role="button" onClick={onLogoutSuccess}>Logout</span>
-              </li>
-            </>
-          )}
-        </ul>
-
+            {authState.id === 0 && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+            {authState.id > 0 && (
+              <>
+                <li className="nav-item">
+                  <span
+                    className="nav-link"
+                    role="button"
+                    onClick={onLogoutSuccess}
+                  >
+                    {authState.username}
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <span
+                    className="nav-link"
+                    role="button"
+                    onClick={onLogoutSuccess}
+                  >
+                    Logout
+                  </span>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
       </nav>
     </div>
